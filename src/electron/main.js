755b9ea367f2faca558d1bd486f539ed1d01693f
smack0007 <zachary.snow@gmail.com>
2021-08-29 20:19:49 +0200

    Reload electron when the go build output changes.

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,23 +1,28 @@
 const { app, BrowserWindow } = require("electron");
+const fs = require("fs");
 const path = require("path");
 
+const HTML_FILE = "index.html";
+let window = null;
+
 function createWindow() {
-  const win = new BrowserWindow({
+  window = new BrowserWindow({
     show: false,
     autoHideMenuBar: true,
     useContentSize: true,
     width: 1024,
     height: 768,
+    resizable: false,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
   });
 
-  win.webContents.openDevTools({ mode: "detach" });
-  win.loadFile("index.html");
+  window.loadFile(HTML_FILE);
+  window.webContents.openDevTools({ mode: "detach" });
 
-  win.once("ready-to-show", () => {
-    win.show();
+  window.once("ready-to-show", () => {
+    window.show();
   });
 }
 
@@ -27,4 +32,27 @@ app.whenReady().then(() => {
 
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit();
+});
+
+fs.watch(__dirname, (eventType, fileName) => {
+  console.info(`watch: ${fileName} => ${eventType}`);
+
+  if (window !== null) {
+    window.loadFile(HTML_FILE);
+  //     if (hotReloadTimeout !== null) {
+  //         clearTimeout(hotReloadTimeout);
+  //         hotReloadTimeout = null;
+  //     }
+
+  //     hotReloadTimeout = (setTimeout(() => {
+  //         if (window !== null) {
+  //             console.info(`Reloading...`);
+
+  //             window.webContents.send(ElectronEvent.hotReload);
+
+  //             window.loadFile(HTML_FILE);
+  //             hotReloadTimeout = null;
+  //         }
+  //     }, 500));
+  }
 });
--- src/main.go
+++ src/main.go
@@ -8,4 +8,5 @@ func main() {
 func draw(graphics GraphicsContext) {
 	graphics.FillRect(Rect{0, 0, 1024, 768}, Color{0, 0, 0, 255});
 	graphics.FillRect(Rect{10, 10, 100, 100}, Color{255, 0, 0, 255});
+	graphics.FillRect(Rect{110, 110, 100, 100}, Color{0, 0, 255, 255});
 }
