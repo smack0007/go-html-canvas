ba701c6d563e1bc65f50f0f7244eb7396ea90005
smack0007 <zachary.snow@gmail.com>
2021-09-08 21:52:40 +0200

    All card images mapped.

--- src/main/main.go
+++ src/main/main.go
@@ -19,9 +19,7 @@ func main() {
 		}
 
 		fmt.Println("Images loaded.")
-		fmt.Println(fmt.Sprintf("Image Size: %d %d", image.Width(), image.Height()))
-
-		fmt.Println(game.ImageOffsets[0]);
+		
 		draw(graphics, image)
 	})()
 
@@ -32,5 +30,7 @@ func draw(graphics GraphicsContext, image Image) {
 	graphics.FillRect(types.Rect{X: 0, Y: 0, Width: 1024, Height: 768}, Color{0, 0, 0, 255})
 	graphics.FillRect(types.Rect{X: 10, Y: 10, Width: 100, Height: 100}, Color{255, 0, 0, 255})
 	graphics.FillRect(types.Rect{X: 110, Y: 110, Width: 100, Height: 100}, Color{0, 0, 255, 255})
-	graphics.DrawSprite(image, game.ImageOffsets[0], types.Rect{X: 0, Y: 0, Width: game.ImageOffsets[0].Width, Height: game.ImageOffsets[0].Height})
+
+	rect := game.CardImageRectangles[game.Card{Suit: game.Hearts, Value: game.Three}]
+	graphics.DrawSprite(image, rect, types.Rect{X: 0, Y: 0, Width: rect.Width, Height: rect.Height})
 }
